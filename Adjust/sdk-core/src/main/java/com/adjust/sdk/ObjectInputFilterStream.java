e39dc04c7b75b0259b9a3e2f76af88c21d16cda2
Shashank Suhane <shashank@adjust.com>
2026-03-02 15:54:46 +0100

    test: add tests for ObjectInputFilterStream

--- Adjust/sdk-core/src/main/java/com/adjust/sdk/ObjectInputFilterStream.java
+++ Adjust/sdk-core/src/main/java/com/adjust/sdk/ObjectInputFilterStream.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ObjectInputFilterStream extends ObjectInputStream {
-    private static final Set<String> ALLOWED_CLASSES = new HashSet<>(Arrays.asList(
+    static final Set<String> ALLOWED_CLASSES = new HashSet<>(Arrays.asList(
             "java.lang.Boolean",
             "java.lang.Double",
             "java.lang.Enum",
