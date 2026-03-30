089c9b59c007c1cf58f441c05f6b7a8cba8fc5da
Shashank Suhane <shashank@adjust.com>
2026-03-30 15:28:53 +0200

    feat: update version number to 5.6.0

--- Adjust/plugins/sdk-plugin-webbridge/src/main/assets/adjust.js
+++ Adjust/plugins/sdk-plugin-webbridge/src/main/assets/adjust.js
@@ -359,7 +359,7 @@ var Adjust = {
         if (this.adjustConfig) {
             return this.adjustConfig.getSdkPrefix();
         } else {
-            return 'web-bridge5.5.1';
+            return 'web-bridge5.6.0';
         }
     },
 
